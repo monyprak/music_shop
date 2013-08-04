2a571e968a2bcd1b64dc2a8d7424162eb4087dd2
Mony Prak <prak.mony@gmail.com>
2013-08-04 15:56:45 +0700

    Use Paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,15 @@ class Product < ActiveRecord::Base
   def self.search_product(name)
     @products = Product.where("name like '%#{name}%'")
   end
+
+
+  has_attached_file :path_img, styles: { small: "140*175"},
+                    url: "/assets/products/:id/:style/:basename.:extension",
+                    path: ":rails_root/public/images/products/:id/:style/:basename.:extension"                        
+
+  validates_attachment_presence :path_img
+  validates_attachment_size :path_img, less_than: 5.megabytes
+  validates_attachment_content_type :path_img, content_type: ['image/jpeg', 'image/png']                   
+
+
 end
